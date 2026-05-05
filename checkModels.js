3da2aa915449a07805bc77f105b186b45ccbe60c
mayankbhagat <bhagatmayank2005@gmail.com>
2026-05-05 13:53:57 +0530

    edit

--- checkModels.js
+++ checkModels.js
@@ -1,12 +1,12 @@
-const url = 'https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyD6cm1XzN-WQY-ib0F_Nrs9zG07WngD3MA';
+const url = 'https://generativelanguage.googleapis.com/v1beta/models?';
 fetch(url)
-  .then(r => r.json())
-  .then(data => {
-    if (data.error) {
-       console.log('API ERROR:', data.error.message);
-    } else {
-       const generateModels = data.models.filter(m => m.supportedGenerationMethods.includes('generateContent'));
-       console.log('Valid models:', generateModels.map(m => m.name.replace('models/', '')).join(', '));
-       console.log('===== END =====');
-    }
-  }).catch(e => console.error(e));
+   .then(r => r.json())
+   .then(data => {
+      if (data.error) {
+         console.log('API ERROR:', data.error.message);
+      } else {
+         const generateModels = data.models.filter(m => m.supportedGenerationMethods.includes('generateContent'));
+         console.log('Valid models:', generateModels.map(m => m.name.replace('models/', '')).join(', '));
+         console.log('===== END =====');
+      }
+   }).catch(e => console.error(e));
